ef6e35f08b4309ba7f70ede6f9cc79b9cda85c46
Ivan G <mr.zero@inbox.ru>
2023-04-14 17:19:03 +0300

    feat: file-names, get-email-domain, hanoi-tower, mac-address

--- src/file-names.js
+++ src/file-names.js
@@ -1,7 +1,7 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * There's a list of file, since two files cannot have equal names,
+ * There's a lisgit pusht of file, since two files cannot have equal names,
  * the one which comes later will have a suffix (k),
  * where k is the smallest integer such that the found name is not used yet.
  *
@@ -15,9 +15,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  names.forEach((name, ind, list) => {
+    let count = 1
+    for (j = ind + 1; j < list.length; j++)
+      if (list[j] === name) {
+        list[j] += `(${count})`
+        count++
+      }
+  })
+return names
 }
 
 module.exports = {
