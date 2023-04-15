ae04cb171f328660b7b261c63140c51eb1d5d158
Ivan G <mr.zero@inbox.ru>
2023-04-15 23:20:49 +0300

    feat: vigenere-cipher, what-season

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,85 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  encrypt(str, key) {
+    if ((!str) || (!key)) {
+      throw new Error ('Incorrect arguments!')
+    }
+    
+    let strArr = str.toUpperCase().split('')
+    let keyArr = []
+    let keyFlow = []
+    const ltr = /[a-z]/i
+    let output = []
+    while (keyArr.length < strArr.length) {
+      keyArr.push(...key.toUpperCase().split(''))
+    } 
+    keyArr.reverse()
+  
+    strArr.forEach(symbol => {
+      if (ltr.test(symbol)) {
+        keyFlow.push(keyArr.pop())
+      } else {
+        keyFlow.push(' ')
+      }
+    })
+  
+    for (let i = 0; i < strArr.length; i++) {
+      let m = strArr[i].charCodeAt() - 65
+      let k = keyFlow[i].charCodeAt() - 65
+      let c
+      if (ltr.test(strArr[i])) {
+        c = (m + k) % 26
+      } else {
+        c = m
+      }
+      output.push(String.fromCharCode(c + 65))
+    }
+  
+      return (this.direct) ? output.join('') : output.reverse().join('')
+    }
+
+    decrypt(str, key) {
+      if ((!str) || (!key)) {
+        throw new Error ('Incorrect arguments!')
+      }
+
+      let strArr = str.toUpperCase().split('')
+      let keyArr = []
+      let keyFlow = []
+      const ltr = /[a-z]/i
+      let output = []
+      while (keyArr.length < strArr.length) {
+        keyArr.push(...key.toUpperCase().split(''))
+      } 
+      keyArr.reverse()
+    
+      strArr.forEach(symbol => {
+        if (ltr.test(symbol)) {
+          keyFlow.push(keyArr.pop())
+        } else {
+          keyFlow.push(' ')
+        }
+      })
+  
+      for (let i = 0; i < strArr.length; i++) {
+        let c = strArr[i].charCodeAt() - 65
+        let k = keyFlow[i].charCodeAt() - 65
+        let m
+        if (ltr.test(strArr[i])) {
+          m = (26 + c - k) % 26
+        } else {
+          m = c
+        }
+        output.push(String.fromCharCode(m + 65))
+      }
+    
+      return (this.direct) ? output.join('') : output.reverse().join('')
+    }
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return ('Unable to determine the time of year!')
+  }
+  if (date.hasOwnProperty('toString')) {
+    throw new Error ('Invalid date!')
+  }
+  if (isNaN(Date.parse(date))) {
+    throw new Error ('Invalid date!')
+  }
+  let output
+  let month = date.getMonth()
+  if ((month < 2) || (month === 11)) {
+    output = 'winter'
+  } else if (month < 5) {
+    output = 'spring'
+  } else if (month < 8) {
+    output = 'summer'
+  } else {
+    output = 'fall'
+  }
+  return output
 }
 
 module.exports = {
